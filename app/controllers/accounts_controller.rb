b3cd729c2c1062da3c8aba6aa865034fc9208a82
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-08-04 15:03:15 +0100

    add error message if enter an incorrect account id

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,8 +2,13 @@ class AccountsController < ApplicationController
 
   def index
     if params[:search]
-      account = Account.find(params[:search])
-      redirect_to account_path(account)
+      account = Account.find_by_id(params[:search])
+      if account == nil
+        flash[:error] = 'There is no Account with that ID.'
+        redirect_to accounts_path
+      else
+        redirect_to account_path(account)
+      end
     end
   end
 
