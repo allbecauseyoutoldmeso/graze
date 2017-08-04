515ea9034c08b72caff91208708b7dab348b288b
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-08-04 11:56:43 +0100

    create rating method for product class too

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ActiveRecord::Base
 
   self.table_name = 'product'
 
+  def ratings
+    [Rating.find_by(product_id: id)]
+  end
+
   has_and_belongs_to_many :boxes, :join_table => :box_to_product
 
 end
